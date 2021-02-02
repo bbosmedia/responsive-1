632b15e4d9227d1fb6abcc45c5054b89e5bc568b
bbosmedia <bbosmedia@users.noreply.github.com>
2021-02-02 22:16:46 +0500

    I added some web designs

--- app.js
+++ app.js
@@ -38,14 +38,13 @@ function scrollActive(){
     const scrollY = window.pageYOffset
 
     sections.forEach(current => {
-        const sectionHeight = current.offsetHeight
+        const sectionHeigh = current.offsetHeight
         const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionHeight + sectionTop){
-            document.querySelector('.nav__menu a[herf*=' + sectionId + ']').classList.add('active')
+        const sectionId = current.getAttribute('id')
+        if(scrollY > sectionHeigh && scrollY <= sectionTop + sectionHeigh){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
         }else{
-            document.querySelector('.nav__menu a[herf*=' + sectionId + ']').classList.remove('active')
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
     })
 }
